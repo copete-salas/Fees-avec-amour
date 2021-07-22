89bcfb541af82b3738ba6a3e57c6dc89379d60b1
copete-salas <yoyo.yoyo.04.yy@gmail.com>
2021-07-22 15:00:33 +0200

    le site commence enfin

---
+++ script.js
@@ -0,0 +1,5 @@
+const alert = () => {
+  alert(
+    "Bonjour, nous vous informont que le site est en coure de création. Vous pouvez quand même le consulter, Ceci dit pour le moment vous ne pouvez pas encore directement commender sur le site et nous nous escusont d'avance pour toute les gènes que cela pourrait éventuellement vous occasionner. Si jamais vous êtes intérressé par un ou plusieurs article, vous pourrez nous en faire par directement par mail, et nous choisirons ensemble la meilleure solution pour pouvoir vous satisfaire, cela reste notre plus grande préocupation. Notre équipe prend soin de ces client et de sa future clientelle, nous vous souhaitons tous ensemble la bienvenue !"
+  );
+};
